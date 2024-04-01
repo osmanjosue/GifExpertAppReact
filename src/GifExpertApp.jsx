dfc9b8b0c11a6b0bbb658806d01557f1cc9f9152
osmanjosue <osmanjosue@gmail.com>
2024-04-01 14:14:46 -0600

    Seccion 6 => Clase: Validar que sean unicos los nombres

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
   const onAddCategory = (newCategory) => {
+
+    if (categories.includes(newCategory)) return;
     
     setCategories([newCategory, ...categories]);
     // setCategories( cat => [...cat, 'Valorant'])
