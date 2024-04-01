e80f2193298980d9ff6bae6c4047482ddbb7129b
osmanjosue <osmanjosue@gmail.com>
2024-04-01 11:29:34 -0600

    Seccion 6 => Clase: Creando una lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
-    return (
-      <>
-        <h1>GifExpertApp</h1>
-      </>
-    );
-  };
-  
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  console.log(categories);
+
+  return (
+    <>
+      {/* titulo */}
+      <h1>GifExpertApp</h1>
+      {/* Input */}
+      {/* Listado de Gif */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{ category }</li>;
+        })}
+      </ol>
+      {/* Gif Item */}
+    </>
+  );
+};
