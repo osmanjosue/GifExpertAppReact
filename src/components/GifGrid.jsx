3730f5dc4f119fceb8d6e23c9e5259e5d79e1480
osmanjosue <osmanjosue@gmail.com>
2024-04-02 15:33:29 -0600

    Seccion 6 => Clase: useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [counter, setCounter] = useState(10);
+
+
+  useEffect(()=>{
+    getGifs(category);
+  }, []);
 
   return (
     <>
       <h3>{category}</h3>
+      <h5>{counter}</h5>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
+      {/* <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol> */}
     </>
   );
 };
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=1TBaZwmLhbZTrrkFGcRRh3AeqExivpos&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=1TBaZwmLhbZTrrkFGcRRh3AeqExivpos&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
